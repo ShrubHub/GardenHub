342788b88581711ddb8061418f4ffdd84e14bc47
ericazaja <ericazaja@gmail.com>
2022-11-15 18:43:21 +0000

    Erica makes sript for phenology ground truthing

---
+++ scripts/phenology/CG_ground_truth.R
@@ -0,0 +1,19 @@
+#### COMMON GARDEN PHENOLOGY GROUND TRUTHING 2022
+#### Script by Erica Zaja, created 15/11/22
+### Last updated: 15/11/22
+
+# 1. LOADING LIBRARIES ----
+library(tidyverse)
+library(viridis)
+library(readr)
+library(readxl)
+library(gridExtra)
+
+# 2. LOADING DATA ----
+
+# Data collected in the common garden in summer 2022 (weekly throughout June-July-August)
+CG_ground_truth <- read_excel("data/phenology/groundtruthing_2022/FINAL_phenocam_groundtruthing_common_garden22_copy5.xlsx")
+
+
+
+
