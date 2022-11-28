1e144d8f0b2ae1c1a210afc452ce84375b43b8c8
madanderson <your_email@example.com>
2022-11-28 12:07:52 -0500

    madi merged all traits!

--- scripts/source_populations.R
+++ scripts/source_populations.R
@@ -15,39 +15,109 @@ SLA_2022 <- read.csv("data/common_garden_2022/field_sla_2022.csv") # source pop 
 source_traits_2017 <- read.csv("data/source_pops/Salix_field_trait_data.csv") # source population from 2013-2017
 
 str(SLA_2022)
-str(source_traits_2017)
-
-source_traits_2017$date <- dmy(source_2017$Date_sampled)
-source_2017$SampleDate <- ymd_hms(source_2017$SampleDate) #  are these columns the same? 
-
+str(source_traits_2017) # why oh why are the dates so terrible 
 
 # minor org on data frames 
+# make separate columns for date  
+SLA_2022$year <-  format(as.Date(SLA_2022$date_sampled, format="%Y-%m-%d"),"%Y")
+SLA_2022$month <-  format(as.Date(SLA_2022$date_sampled, format="%Y-%m-%d"),"%m")
+SLA_2022$DOY <-  yday(as.POSIXct(SLA_2022$date_sampled, format = "%Y-%m-%d"))
+
 SLA_2022 <- SLA_2022 %>% 
   mutate(Site = recode(Site, "Kluane Plateau" = 'Kluane', "Qikiqtaruk" = 'Qikiqtaruk')) %>% 
-  dplyr::select(-c(X, rehydrated_leaf_sample_remarks, sample_remarks, dried_leaf_sample_remarks)) %>% 
-  filter(date_sampled != "2022-08-10" & date_sampled !="2022-08-10" & # filter out august observations to avoid any senescence 
-           date_sampled != "2022-08-07")
+  filter(month != "08")  %>%   #filter out august observations to avoid any senescence 
+ mutate(total_leaf_dry_mass_mg = total_leaf_dry_mass_g*1000) %>% # convert leaf mass per area to mg instead of g 
+  mutate(LDMC_g_g = LDMC/1000) %>% # covert LDMC from mg g-1 to g g-1
+  dplyr::select(-c(X, LDMC, rehydrated_leaf_sample_remarks, total_leaf_dry_mass_g, sample_remarks, dried_leaf_sample_remarks))  
 
 source_traits_2017 <- source_traits_2017 %>% 
-  dplyr::select(-Treatment)
-
+  dplyr::select(-Treatment) # honestly date info in this sheet is such a nightmare, just focus on year
   
-all_source_2022 <- full_join(SLA_2022, source_traits_2017, by = c("Site" = "Site", 
+str(source_traits_2017)
+str(SLA_2022)
+SLA_2022$DOY <- as.character(SLA_2022$DOY) # converting to character to merge because source pop data is so messed up
+SLA_2022$year <- as.integer(SLA_2022$DOY) # converting to same variable type for merging 
+
+# merge! 
+all_source_traits_2022 <- full_join(SLA_2022, source_traits_2017, by = c("Site" = "Site", 
                                                            "Species" = "Species", 
-                                                           "X" = "X",
-                                                           "" = "",
+                                                           "DOY" = "DOY",
+                                                           "year" = "Year_measured",
+                                                           "LA" = "Leaf_area_mm2", 
+                                                           "SLA" = "SLA_mm2_mg", 
+                                                           "LDMC_g_g" = "LDMC_g_g", 
+                                                           "total_leaf_dry_mass_mg" = "Leaf_dry_mass_mg", 
+                                                           "leaf_fresh_mass_g" = "Leaf_fresh_mass_g", 
+                                                           "site_id" = "Subsite_name"
                                                            )) 
-  # select(-c(X, sample_remarks, rehydrated_leaf_sample_remarks, dried_leaf_sample_remarks, -Match))
   
 
+# all years plots ---- 
+(sla_all_source <- ggplot(all_source_traits_2022) +
+   geom_boxplot(aes(x = Site, y = SLA, colour = Site, fill = Site, group = Site), size = 0.5, alpha = 0.5) +
+   facet_grid(cols = vars(Species)) +
+   ylab("SLA (mm2mg-1)") +
+   xlab("Source population") +
+   scale_colour_viridis_d(begin = 0.1, end = 0.85) +
+   scale_fill_viridis_d(begin = 0.1, end = 0.85) +
+   theme_bw() +
+   theme(panel.border = element_blank(),
+         panel.grid.major = element_blank(),
+         panel.grid.minor = element_blank(),
+         strip.text = element_text(size = 15, color = "black", face = "italic"),
+         legend.title = element_text(size=15), #change legend title font size
+         legend.text = element_text(size=12),
+         axis.line = element_line(colour = "black"),
+         axis.title = element_text(size = 14),
+         axis.text.x = element_text(vjust = 0.5, size = 12, colour = "black"),
+         axis.text.y = element_text(size = 12, colour = "black")))
 
-# 2022 plots ====
+# Leaf area 
+(la_source_all <- ggplot(all_source_traits_2022) +
+    geom_boxplot(aes(x = Site, y = LA, colour = Site, fill = Site, group = Site), size = 0.5, alpha = 0.5) +
+    facet_grid(cols = vars(Species)) +
+    ylab("LA") +
+    xlab("") +
+    scale_colour_viridis_d(begin = 0.1, end = 0.85) +
+    scale_fill_viridis_d(begin = 0.1, end = 0.85) +
+    theme_bw() +
+    theme(panel.border = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          strip.text = element_text(size = 15, color = "black", face = "italic"),
+          legend.title = element_text(size=15), #change legend title font size
+          legend.text = element_text(size=12),
+          axis.line = element_line(colour = "black"),
+          axis.title = element_text(size = 14),
+          axis.text.x = element_text(vjust = 0.5, size = 12, colour = "black"),
+          axis.text.y = element_text(size = 12, colour = "black")))
+
+(ldmc_source_all <- ggplot(all_source_traits_2022) +
+    geom_boxplot(aes(x = Site, y = LDMC_g_g, colour = Site, fill = Site, group = Site), size = 0.5, alpha = 0.5) +
+    facet_grid(cols = vars(Species)) +
+    ylab("LDMC") +
+    xlab("") +
+    scale_colour_viridis_d(begin = 0.1, end = 0.85) +
+    scale_fill_viridis_d(begin = 0.1, end = 0.85) +
+    theme_bw() +
+    theme(panel.border = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          strip.text = element_text(size = 15, color = "black", face = "italic"),
+          legend.title = element_text(size=15), #change legend title font size
+          legend.text = element_text(size=12),
+          axis.line = element_line(colour = "black"),
+          axis.title = element_text(size = 14),
+          axis.text.x = element_text(vjust = 0.5, size = 12, colour = "black"),
+          axis.text.y = element_text(size = 12, colour = "black")))
+
+# only 2022 plots ====
 # quick plot of ALL source populations SLA 
 # field data collected 2021 for Kluane and 2022 for QHI
  # but, to be fair this includes senescing leaves, too 
  # samples in Klaune were collected 4-5  times over season - should probably pick a July value
  # samples in QHI were collected in July or senescing in Aug. 
-(sla_source <- ggplot(SLA_2022) +
+(sla_2022_source <- ggplot(SLA_2022) +
     geom_boxplot(aes(x = Site, y = SLA, colour = Site, fill = Site, group = Site), size = 0.5, alpha = 0.5) +
     facet_grid(cols = vars(Species)) +
     ylab("SLA (mm2mg-1)") +
@@ -93,7 +163,7 @@ july_sla <- SLA_2022 %>%
 
 
 # Leaf area 
-(sla_source_july <- ggplot(july_sla) +
+(la_source_july <- ggplot(july_sla) +
     geom_boxplot(aes(x = Site, y = LA, colour = Site, fill = Site, group = Site), size = 0.5, alpha = 0.5) +
     facet_grid(cols = vars(Species)) +
     ylab("LA") +
