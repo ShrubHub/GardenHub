1fbe610037d9afc0110dde1d87376c7b7cc50676
madanderson <your_email@example.com>
2022-11-28 10:06:45 -0500

    madi formatting source pop trait data to merge

--- scripts/source_populations.R
+++ scripts/source_populations.R
@@ -1,19 +1,47 @@
 #### Source populations
 # Exploration of southern (Kluane) and northern (Qikiqtaruk) willow populations 
-# Madelaine 16/11/22
+# Madelaine 28/11/22
 # last updated: 
 
 # libraries ---- 
 library(ggplot2)
 library(viridis)
+library(lubridate)
 library(dplyr)
 
 
 # data ---- 
-SLA_2022 <- read.csv("data/common_garden_2022/field_sla_2022.csv")
+SLA_2022 <- read.csv("data/common_garden_2022/field_sla_2022.csv") # source pop data from 2021-22
+source_traits_2017 <- read.csv("data/source_pops/Salix_field_trait_data.csv") # source population from 2013-2017
 
 str(SLA_2022)
+str(source_traits_2017)
 
+source_traits_2017$date <- dmy(source_2017$Date_sampled)
+source_2017$SampleDate <- ymd_hms(source_2017$SampleDate) #  are these columns the same? 
+
+
+# minor org on data frames 
+SLA_2022 <- SLA_2022 %>% 
+  mutate(Site = recode(Site, "Kluane Plateau" = 'Kluane', "Qikiqtaruk" = 'Qikiqtaruk')) %>% 
+  dplyr::select(-c(X, rehydrated_leaf_sample_remarks, sample_remarks, dried_leaf_sample_remarks)) %>% 
+  filter(date_sampled != "2022-08-10" & date_sampled !="2022-08-10" & # filter out august observations to avoid any senescence 
+           date_sampled != "2022-08-07")
+
+source_traits_2017 <- source_traits_2017 %>% 
+  dplyr::select(-Treatment)
+
+  
+all_source_2022 <- full_join(SLA_2022, source_traits_2017, by = c("Site" = "Site", 
+                                                           "Species" = "Species", 
+                                                           "X" = "X",
+                                                           "" = "",
+                                                           )) 
+  # select(-c(X, sample_remarks, rehydrated_leaf_sample_remarks, dried_leaf_sample_remarks, -Match))
+  
+
+
+# 2022 plots ====
 # quick plot of ALL source populations SLA 
 # field data collected 2021 for Kluane and 2022 for QHI
  # but, to be fair this includes senescing leaves, too 
