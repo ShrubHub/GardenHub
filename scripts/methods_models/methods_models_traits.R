a145bccf6a0b461bf03f74a9b935074dd55b8fbf
madanderson <your_email@example.com>
2022-11-29 12:35:14 -0500

    madi made methods trait script

---
+++ scripts/methods_models/methods_models_traits.R
@@ -0,0 +1,17 @@
+# methods models for trait differences comparing northern and southern willows
+# Created 29 Nov 2022 Madelaine 
+# last updated: 29/11/22
+
+# traits: SLA, LDMC, LA, leaf length, stem diameter, canopy height, shrub width
+
+# libraries ----
+
+# data ---- 
+
+
+# SLA ----
+
+# LDMC ---- 
+
+# LA ---- 
+
