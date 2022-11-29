72e86434c5b7abb42ca35828bac447b341e26084
madanderson <your_email@example.com>
2022-11-29 12:51:28 -0500

    madi starting method model structure for traits

--- scripts/methods_models/methods_models_traits.R
+++ scripts/methods_models/methods_models_traits.R
@@ -3,13 +3,25 @@
 # last updated: 29/11/22
 
 # traits: SLA, LDMC, LA, leaf length, stem diameter, canopy height, shrub width
+# model structure: 
+#  lmer(SLA ~ site*species +  (1|sample_year/field_sample_ID))
+# where site is Kluane or Qikiqtaruk 
+
 
 # libraries ----
+library(lme4)
+
 
 # data ---- 
+# SLA, LDMC, LA: 
+all_source_area_traits <- read.csv("data/source_pops/all_source_area_traits.csv")
 
 
 # SLA ----
+str(all_source_area_traits)
+
+SLA_method_mod <- lmer(SLA ~ Site*Species + (1|year/))
+
 
 # LDMC ---- 
 
