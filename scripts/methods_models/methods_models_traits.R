c402f98c1b6c48c82810594634a397cd81c96bee
madanderson <your_email@example.com>
2022-12-02 10:52:44 -0500

    mad changing random effect structure for methods models

--- scripts/methods_models/methods_models_traits.R
+++ scripts/methods_models/methods_models_traits.R
@@ -5,6 +5,8 @@
 # traits: SLA, LDMC, LA, leaf length, stem diameter, shrub width
 # model structure: 
 #  lmer(SLA ~ site*species +  (1|sample_year/field_sample_ID))
+#  OR, having species as a random effect: 
+# lmer(SLA ~ site + (1|sample_year) + (1|species))
 # where site is Kluane or Qikiqtaruk 
 
 
@@ -38,6 +40,14 @@ plot(SLA_method_mod)
 qqnorm(resid(SLA_method_mod))
 qqline(resid(SLA_method_mod)) 
 
+# okay, do we want species as an interaction term? not our main q and our other models aren't including it
+# here, as random effect: 
+SLA_method_mod_spp <- lmer(SLA ~ Site + (1|year) + (1|Species), data = all_source_area_traits) 
+summary(SLA_method_mod_spp)
+plot(SLA_method_mod_spp)
+qqnorm(resid(SLA_method_mod_spp))
+qqline(resid(SLA_method_mod_spp))
+
 # LDMC ---- 
 # same comments as above (SLA)
 LDMC_method_mod <- lmer(LDMC_g_g ~ Site*Species + (1|year), data = all_source_area_traits) 
@@ -46,6 +56,13 @@ plot(LDMC_method_mod)
 qqnorm(resid(LDMC_method_mod))
 qqline(resid(LDMC_method_mod)) 
 
+# species as random effect 
+LDMC_method_mod_spp <- lmer(LDMC_g_g ~ Site + (1|year) + (1|Species), data = all_source_area_traits) 
+summary(LDMC_method_mod_spp)
+plot(LDMC_method_mod_spp)
+qqnorm(resid(LDMC_method_mod_spp))
+qqline(resid(LDMC_method_mod_spp)) 
+
 # LA ---- 
 LA_method_mod <- lmer(LA ~ Site*Species + (1|year), data = all_source_area_traits) 
 summary(LA_method_mod)
@@ -53,19 +70,32 @@ plot(LA_method_mod)
 qqnorm(resid(LA_method_mod))
 qqline(resid(LA_method_mod)) 
 
+# species as random effect 
+LA_method_mod_spp <- lmer(LA ~ Site + (1|year) + (1|Species), data = all_source_area_traits) 
+summary(LA_method_mod_spp)
+plot(LA_method_mod_spp)
+qqnorm(resid(LA_method_mod_spp))
+qqline(resid(LA_method_mod_spp)) 
+
 # leaf length (LL) ----
 # use mean value from 3 leaf lengths 
 LL_method_mod <- lmer(mean_leaf_length ~ Site*Species + (1|SampleYear), data = unique_source_mother) 
-LL_method_mod_spp <- lmer(mean_leaf_length ~ Site + (1|Species) + (1|SampleYear), data = unique_source_mother) 
+# note: fixed-effect model matrix is rank deficient so dropping 1 column / coefficient
+summary(LL_method_mod) 
+plot(LL_method_mod)
+qqnorm(resid(LL_method_mod))
+qqline(resid(LL_method_mod))
 
-summary(LL_method_mod_nospp) # fixed-effect model matrix is rank deficient so dropping 1 column / coefficient
-plot(LL_method_mod_nospp)
-qqnorm(resid(LL_method_mod_nospp))
-qqline(resid(LL_method_mod_nospp))
+# species as random effect 
+LL_method_mod_spp <- lmer(mean_leaf_length ~ Site + (1|Species) + (1|SampleYear), data = unique_source_mother) 
+summary(LL_method_mod_spp) 
+plot(LL_method_mod_spp)
+qqnorm(resid(LL_method_mod_spp))
+qqline(resid(LL_method_mod_spp))
 
 
 # shrub width ---- 
-
+# oh it seems as though Erica has run this and stem diameter 
 # 
 
 
