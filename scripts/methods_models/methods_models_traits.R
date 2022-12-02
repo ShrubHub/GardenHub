dbdf217845a69bd863f0550ee3148f746c323699
madanderson <your_email@example.com>
2022-12-02 10:36:47 -0500

    mad making methods models

--- scripts/methods_models/methods_models_traits.R
+++ scripts/methods_models/methods_models_traits.R
@@ -2,7 +2,7 @@
 # Created 29 Nov 2022 Madelaine 
 # last updated: 29/11/22
 
-# traits: SLA, LDMC, LA, leaf length, stem diameter, canopy height, shrub width
+# traits: SLA, LDMC, LA, leaf length, stem diameter, shrub width
 # model structure: 
 #  lmer(SLA ~ site*species +  (1|sample_year/field_sample_ID))
 # where site is Kluane or Qikiqtaruk 
@@ -10,15 +10,27 @@
 
 # libraries ----
 library(lme4)
-
+library(dplyr)
 
 # data ---- 
 # SLA, LDMC, LA: 
 all_source_area_traits <- read.csv("data/source_pops/all_source_area_traits.csv")
+# leaf length, stem diameter, width 
+unique_source_mother <-  read.csv("data/source_pops/unique_source_mother.csv")
 
+str(all_source_area_traits)
+# reclass variables 
+all_source_area_traits$Species <- as.factor(all_source_area_traits$Species)
+all_source_area_traits$Site <- as.factor(all_source_area_traits$Site)
+all_source_area_traits$year <- as.factor(all_source_area_traits$year)
+
+str(unique_source_mother) 
+# reclass variables 
+unique_source_mother$Species <- as.factor(unique_source_mother$Species)
+unique_source_mother$Site <- as.factor(unique_source_mother$Site)
+unique_source_mother$SampleYear <- as.factor(unique_source_mother$SampleYear)
 
 # SLA ----
-str(all_source_area_traits)
 # not including field_sample_id because those don't exist for any values prior to 2021 ? and not repeated measures anyways 
 SLA_method_mod <- lmer(SLA ~ Site*Species + (1|year), data = all_source_area_traits) 
 summary(SLA_method_mod)
@@ -27,6 +39,33 @@ qqnorm(resid(SLA_method_mod))
 qqline(resid(SLA_method_mod)) 
 
 # LDMC ---- 
+# same comments as above (SLA)
+LDMC_method_mod <- lmer(LDMC_g_g ~ Site*Species + (1|year), data = all_source_area_traits) 
+summary(LDMC_method_mod)
+plot(LDMC_method_mod)
+qqnorm(resid(LDMC_method_mod))
+qqline(resid(LDMC_method_mod)) 
 
 # LA ---- 
+LA_method_mod <- lmer(LA ~ Site*Species + (1|year), data = all_source_area_traits) 
+summary(LA_method_mod)
+plot(LA_method_mod)
+qqnorm(resid(LA_method_mod))
+qqline(resid(LA_method_mod)) 
+
+# leaf length (LL) ----
+# use mean value from 3 leaf lengths 
+LL_method_mod <- lmer(mean_leaf_length ~ Site*Species + (1|SampleYear), data = unique_source_mother) 
+LL_method_mod_spp <- lmer(mean_leaf_length ~ Site + (1|Species) + (1|SampleYear), data = unique_source_mother) 
+
+summary(LL_method_mod_nospp) # fixed-effect model matrix is rank deficient so dropping 1 column / coefficient
+plot(LL_method_mod_nospp)
+qqnorm(resid(LL_method_mod_nospp))
+qqline(resid(LL_method_mod_nospp))
+
+
+# shrub width ---- 
+
+# 
+
 
