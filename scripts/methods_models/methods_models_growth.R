fb33cab45534211af6171f410c209b00dacfa5ac
ericazaja <ericazaja@gmail.com>
2022-12-05 12:20:45 +0000

    Erica runs all methods growth models

--- scripts/methods_models/methods_models_growth.R
+++ scripts/methods_models/methods_models_growth.R
@@ -13,17 +13,19 @@ library(lme4)
 # 2. Loading data ---- 
 unique_source_mother <- read_csv("data/source_pops/unique_source_mother.csv")
 
-# 3. Modelling ----
-# a. Canopy height (cm) -----
-
+# 3. Wrangling ----
 # variables in right format
 str(unique_source_mother)
-unique_source_mother$SampleYear <- as.factor(july_source_pop_plus_mother$SampleYear)
+unique_source_mother$SampleYear <- as.factor(unique_source_mother$SampleYear)
 unique_source_mother$Species <- as.factor(unique_source_mother$Species)
 unique_source_mother$Site <- as.factor(unique_source_mother$Site)
 unique(unique_source_mother$SampleYear)
 
-# Running lmer
+# 4. Modelling ----
+
+# a. Canopy height -----
+
+# Height lmer with species interacting
 height_method_mod <- lmer(Canopy_Height_cm ~ Site*Species + (1|SampleYear), data = unique_source_mother)
 
 summary(height_method_mod)
@@ -31,11 +33,16 @@ plot(height_method_mod)
 qqnorm(resid(height_method_mod))
 qqline(resid(height_method_mod)) 
 
-# b. Stem elongation ---- 
-#unique_source_mother_elong <- unique_source_mother %>%
- #select(mean_stem_elong, Site, Species, SampleYear, SampleID) %>%
-  #drop_na(mean_stem_elong)
+# Height lmer with species random effect
+height_method_mod_2 <- lmer(Canopy_Height_cm ~ Site + (1|Species) + (1|SampleYear), data = unique_source_mother)
+summary(height_method_mod_2)
+plot(height_method_mod_2)
+qqnorm(resid(height_method_mod_2))
+qqline(resid(height_method_mod_2)) 
 
+# b. Stem elongation ---- 
+
+# Stem elongation lmer with species interacting
 stem_elong_method_mod <- lmer(mean_stem_elong ~ Site*Species + (1|SampleYear), data = unique_source_mother)
 # fixed-effect model matrix is rank deficient so dropping 1 column / coefficient?
 summary(stem_elong_method_mod)
@@ -43,22 +50,49 @@ plot(stem_elong_method_mod)
 qqnorm(resid(stem_elong_method_mod))
 qqline(resid(stem_elong_method_mod)) 
 
+# Stem elongation lmer with species random effect
+stem_elong_method_mod_2 <- lmer(mean_stem_elong ~ Site + (1|Species) + (1|SampleYear), data = unique_source_mother)
+#no warning for this 
+summary(stem_elong_method_mod_2)
+plot(stem_elong_method_mod_2)
+qqnorm(resid(stem_elong_method_mod_2))
+qqline(resid(stem_elong_method_mod_2)) 
+
 # c. Width---- 
+
+# Width lmer with species interacting
 width_method_mod <- lmer(mean_width ~ Site*Species + (1|SampleYear), data = unique_source_mother)
+
 summary(width_method_mod)
 plot(width_method_mod)
-qqnorm(resid(width_method_mod ))
-qqline(resid(width_method_mod )) 
+qqnorm(resid(width_method_mod))
+qqline(resid(width_method_mod)) 
 
-str(unique_source_mother)
+# Width lmer with species random effect
+width_method_mod_2 <- lmer(mean_width ~ Site + (1|Species) + (1|SampleYear), data = unique_source_mother)
+
+summary(width_method_mod_2)
+plot(width_method_mod_2)
+qqnorm(resid(width_method_mod_2))
+qqline(resid(width_method_mod_2)) 
 
 # d. Stem diameter -----
-unique_source_mother_diam <- unique_source_mother %>%
-select(Stem_diameter, Site, Species, SampleYear, SampleID) %>%
-drop_na(Stem_diameter)
 
-unique(unique_source_mother_diam$Site)
-# where did all the kluane stem diameters go?
-
+# Diameter lmer with species interacting
 diam_method_mod <- lm(Stem_diameter ~ Site*Species, data = unique_source_mother)
 # removing sample year random effect because only data from 2022
+
+summary(diam_method_mod)
+plot(diam_method_mod)
+qqnorm(resid(diam_method_mod))
+qqline(resid(diam_method_mod)) 
+
+# Diameter lmer with species random effect
+diam_method_mod_2 <- lmer(Stem_diameter ~ Site + (1|Species), data = unique_source_mother)
+# removing sample year random effect because only data from 2022
+# boundary (singular) fit: see help('isSingular')
+summary(diam_method_mod_2)
+plot(diam_method_mod_2)
+qqnorm(resid(diam_method_mod_2))
+qqline(resid(diam_method_mod_2)) 
+
