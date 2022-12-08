de385f35ebd3b3be994043a9aa6eb6dedc4f981d
madanderson <your_email@example.com>
2022-12-08 17:37:41 -0500

    set up environment conditions script

---
+++ scripts/environment/QHI_env.R
@@ -0,0 +1,18 @@
+### QHI environmental conditions
+# Script by Madi 08/12/2022
+# last updated: 
+
+# TOMST ----
+# from 2022 27 July - 16 August 2022 
+
+# data largely wrangled already 
+
+tomst_QHI_mean_daily_temp <- read.csv("data/tomst/QHI_TOMST_August2022/QHI_mean_daily_temp.csv")
+
+
+# HOBO ---- 
+# data from 2022 
+# data from 2017
+
+# iButtons ---- 
+# eclectic mix of data  
