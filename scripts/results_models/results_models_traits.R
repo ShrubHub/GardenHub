e7c1cfa873705da6ce8d0f7f0c0026f412f89c46
madanderson <your_email@example.com>
2022-12-13 08:22:08 -0500

    made script, didn't do anything with it

---
+++ scripts/results_models/results_models_traits.R
@@ -0,0 +1,8 @@
+# Results models for trait differences comparing northern and southern willows
+# Mad 13/12/2022
+# Last updated: 13/12/2022
+
+# traits: SLA, LDMC, LA? (currently not possible), leaf length  
+
+# model, where Site is Kluane or QHI 
+# lmer(TRAIT ~ )
