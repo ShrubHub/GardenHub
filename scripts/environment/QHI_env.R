ac4f378f0b3fc71ef54bef7a97c1d302dd54eb91
madanderson <your_email@example.com>
2022-12-14 11:11:05 -0500

    organizing QHI enviro data

--- scripts/environment/QHI_env.R
+++ scripts/environment/QHI_env.R
@@ -1,6 +1,13 @@
 ### QHI environmental conditions
 # Script by Madi 08/12/2022
-# last updated: 
+# last updated: 14/12/2022
+
+# LIBRARIES ---- 
+
+library(dplyr)
+library(ggplot2)
+library(ggpubr)
+library(lubridate)
 
 # TOMST ----
 # from 2022 27 July - 16 August 2022 
@@ -12,7 +19,8 @@ tomst_QHI_mean_daily_temp <- read.csv("data/tomst/QHI_TOMST_August2022/QHI_mean_
 
 # HOBO ---- 
 # data from 2022 
-# data from 2017
+# data from 2017-18 
 
 # iButtons ---- 
+# ignore for now 
 # eclectic mix of data  
--- scripts/results_models/results_models_traits.R
+++ scripts/results_models/results_models_traits.R
@@ -1,6 +1,6 @@
 # Results models for trait differences comparing northern and southern willows
 # Mad 13/12/2022
-# Last updated: 13/12/2022
+# Last updated: 14/12/2022
 
 # traits: SLA, LDMC, LA? (currently not possible), leaf length  
 
