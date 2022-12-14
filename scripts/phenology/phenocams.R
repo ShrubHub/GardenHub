af4813b2838fbc260e93c98015def4812e1b6383
@ericazaja <ericazaja@gmail.com>
2022-12-14 14:47:20 +0000

    E started script to analyse phenocam data

--- scripts/phenology/phenocams.R
+++ scripts/phenology/phenocams.R
@@ -14,3 +14,13 @@ KP_phenocams_2021 <- read_csv("data/phenology/phenocam_pics/KP_phenocams_2021.cs
 QHI_phenocams_2022 <- read_csv("data/phenology/phenocam_pics/QHI_phenocams_2022.csv")
 KP_phenocams_2022 <- read_csv("data/phenology/phenocam_pics/KP_phenocams_2022.csv")
 Phenocam_Datasheet_QHI <- read_excel("data/phenology/phenocam_pics/Phenocam_Datasheet_QHI.xlsx")
+# add common garden datasheet when available
+
+
+# 3. DATA WRANGLING-----
+
+KP_phenocams_2022 <-KP_phenocams_2022[1:9,] # removing loads of NAs
+KP_phenocams_2021 <- KP_phenocams_2021[1:9,]
+
+# merge 2021 and 2022 kp datasets?
+
