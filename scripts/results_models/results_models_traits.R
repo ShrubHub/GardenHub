23375361225863ffd326c6c7161e06a7344a82e5
madanderson <your_email@example.com>
2022-12-17 11:11:18 -0500

    rerunning trait models without Site

--- scripts/results_models/results_models_traits.R
+++ scripts/results_models/results_models_traits.R
@@ -1,12 +1,11 @@
 # Results models for trait differences comparing northern and southern willows
 # Mad 13/12/2022
-# Last updated: 14/12/2022
+# Last updated: 17/12/2022
 
-# traits: SLA, LDMC, LA? (currently not possible), leaf length  
+# traits: SLA, LDMC, LA, leaf length  
 
-# model, where Site is Kluane or QHI 
-# lmer(TRAIT ~ population*site + (1|sample_year/species/sample_ID))
-# where population = northern or southern pop in garden and site = source QHI or KLU
+# model, where population is CG northern, CG southern, source northern (QHI), source southern (Kluane alpine)
+# lmer(TRAIT ~ population + (1|sample_year/species/sample_ID))
 # need to make sample_year column, although not critical for leaf traits? 
 # separate nested random effects if it doesn't converge 
 
@@ -26,10 +25,8 @@ str(all_CG_source_traits)
 str(all_CG_source_growth)
 
 # reclass variables 
-all_CG_source_growth$Species <- as.factor(all_CG_source_growth$Species)
 all_CG_source_growth$SampleID_standard <- as.factor(all_CG_source_growth$SampleID_standard)
 all_CG_source_growth$population <- as.factor(all_CG_source_growth$population)
-all_CG_source_growth$Site <- as.factor(all_CG_source_growth$Site)
 all_CG_source_growth$Sample_Date <- as.POSIXct(all_CG_source_growth$Sample_Date, format = '%Y/%m/%d')
 all_CG_source_growth$Year <- as.factor(all_CG_source_growth$Year)
 all_CG_source_growth$Sample_age <- as.factor(all_CG_source_growth$Sample_age)
@@ -37,22 +34,20 @@ all_CG_source_growth$Sample_age <- as.factor(all_CG_source_growth$Sample_age)
 all_CG_source_traits$Species <- as.factor(all_CG_source_traits$Species)
 all_CG_source_traits$plant_tag_id <- as.factor(all_CG_source_traits$plant_tag_id)
 all_CG_source_traits$population <- as.factor(all_CG_source_traits$population)
-all_CG_source_traits$Site <- as.factor(all_CG_source_traits$Site)
 all_CG_source_traits$date_sampled <- as.POSIXct(all_CG_source_traits$date_sampled, format = '%Y-%m-%d')
 all_CG_source_traits$year <- as.factor(all_CG_source_traits$year)
 # currently doesn't exist! all_CG_source_traits$Sample_age <- as.factor(all_CG_source_traits$Sample_age)
 
 # SLA ----
-SLA_mod_1 <- lmer(SLA ~ population*Site + (1|year/Species/plant_tag_id), 
+SLA_mod_1 <- lmer(SLA ~ population + (1|year/Species/plant_tag_id), 
                  data = all_CG_source_traits)
-# fixed-effect model matrix is rank deficient so dropping 5 columns / coefficients
 # boundary (singular) fit: see help('isSingular')
 summary(SLA_mod_1)
 tab_model(SLA_mod_1)
 
 # dropping plant_tag_id because shrubs weren't repeatedly measured either in source pop or garden
 # in 2021 and 2022, same shrubs were sampled in garden, but also diff than in 2017 
-SLA_mod_2 <- lmer(SLA ~ population*Site + (1|year/Species), 
+SLA_mod_2 <- lmer(SLA ~ population + (1|year/Species), 
                   data = all_CG_source_traits)
 # fixed-effect model matrix is rank deficient so dropping 5 columns / coefficients
 summary(SLA_mod_2)
@@ -61,9 +56,8 @@ tab_model(SLA_mod_2)
 # LDMC ---- 
 # same as SLA so not including plant_tag_id because shrubs weren't repeatedly measured either in source pop or garden
 # in 2021 and 2022, same shrubs were sampled in garden, but also diff than in 2017 
-LDMC_mod_1 <- lmer(LDMC_g_g ~ population*Site + (1|year/Species), 
+LDMC_mod_1 <- lmer(LDMC_g_g ~ population + (1|year/Species), 
                   data = all_CG_source_traits)
-# fixed-effect model matrix is rank deficient so dropping 5 columns / coefficients
 summary(LDMC_mod_1)
 tab_model(LDMC_mod_1)
 
@@ -71,9 +65,8 @@ tab_model(LDMC_mod_1)
 
 # LEAF LENGTH ---- 
 # not including sample age as fixed effect (or random) for leaf traits 
-ll_mod_1 <- lmer(mean_leaf_length ~ population*Site + (1|Year/Species/SampleID_standard), 
+ll_mod_1 <- lmer(mean_leaf_length ~ population + (1|Year/Species/SampleID_standard), 
                data = all_CG_source_growth)
-# fixed-effect model matrix is rank deficient so dropping 5 columns / coefficients 
 summary(ll_mod_1)
 tab_model(ll_mod_1)
 
@@ -86,22 +79,20 @@ all_CG_source_traits_2022 <- all_CG_source_traits %>%
 
 # same model srtucture as above. No sample age or ID 
 # removing year as random effect because only two years worth of data 
-LA_mod_1 <- lmer(LA ~ population*Site + (1|Species), 
+LA_mod_1 <- lmer(LA ~ population + (1|Species), 
                    data = all_CG_source_traits_2022)
-# fixed-effect model matrix is rank deficient so dropping 5 columns / coefficients
 # small sample size 
 summary(LA_mod_1)
 tab_model(LA_mod_1)
 
 # LMA ---- 
 # same model as SLA, LDMC, leaf length  
-LMA_mod_1 <- lmer(leaf_mass_per_area_g_m2 ~ population*Site + (1|year/Species), 
+LMA_mod_1 <- lmer(leaf_mass_per_area_g_m2 ~ population + (1|year/Species), 
                  data = all_CG_source_traits)
-# fixed-effect model matrix is rank deficient so dropping 5 columns / coefficients
 # boundary (singular) fit: see help('isSingular')
 
 # removing year as nested random effect:
-LMA_mod_2 <- lmer(leaf_mass_per_area_g_m2 ~ population*Site + (1|Species) + (1|year), 
+LMA_mod_2 <- lmer(leaf_mass_per_area_g_m2 ~ population + (1|Species) + (1|year), 
                   data = all_CG_source_traits)
 # fixed-effect model matrix is rank deficient so dropping 5 columns / coefficients
 summary(LMA_mod_2)
