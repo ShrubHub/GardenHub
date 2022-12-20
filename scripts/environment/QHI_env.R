940646eb1c8bf126b1fdd626c421b053d2bcdf86
madanderson <your_email@example.com>
2022-12-20 10:59:49 +0100

    working on enviro QHI data

--- scripts/environment/QHI_env.R
+++ scripts/environment/QHI_env.R
@@ -15,13 +15,12 @@ library(tidyverse)
 # from 2022 27 July - 16 August 2022 
 
 # data largely wrangled already 
-
-tomst_QHI_mean_daily_temp <- read.csv("data/tomst/QHI_TOMST_August2022/QHI_mean_daily_temp.csv")
-
+# tomst_QHI_mean_daily_temp <- read.csv("data/tomst/QHI_TOMST_August2022/QHI_mean_daily_temp.csv")
 
 # HOBO ---- 
 # data from 2022 
 # data collection and processing by Elise Gallois 
+# script adapted from Elise Gallois truly she did it all
 hobo_QHI_2022 <- read.csv("data/hobo/QHI_FullHOBO_2022.csv")
 
 hobo_summary <- ggplot(hobo_QHI_2022, aes(x = Datetime_UTC, y = Value)) +
@@ -30,21 +29,9 @@ hobo_summary <- ggplot(hobo_QHI_2022, aes(x = Datetime_UTC, y = Value)) +
   theme_classic()
 
 hobo_summary + theme(legend.position = "none")
-
-# historgram of variables
-hist_hobo <- ggplot(hobo_QHI_2022) +
-  aes(x = Value, fill = SerialID) +
-  geom_histogram(bins = 30L) +
-  scale_fill_hue() +
-  theme_minimal() +
-  facet_wrap(vars(Variable), scales = "free")
-
-hobo_sort <- hobo_QHI_2022 %>% 
-  mutate(Date = lubridate::parse_date_time(Datetime_UTC, orders = c("%m/%d/%Y")))
-
-
 str(hobo_QHI_2022)
 
-# iButtons ---- 
-# ignore for now 
-# eclectic mix of data  
+hobo_clean_2022 <- hobo_QHI_2022 %>% 
+  filter(Variable == "Temperature") %>% 
+  select(-c(End.of.File, Button.Down, Host.Connected, Button.Up, Index)) 
+
