24ff23cfd1a2e09c983eda76ac5a962045299edd
madanderson <your_email@example.com>
2022-12-21 09:42:51 +0100

    HOBO 2022 data

--- scripts/environment/QHI_env.R
+++ scripts/environment/QHI_env.R
@@ -22,16 +22,26 @@ library(tidyverse)
 # data collection and processing by Elise Gallois 
 # script adapted from Elise Gallois truly she did it all
 hobo_QHI_2022 <- read.csv("data/hobo/QHI_FullHOBO_2022.csv")
+hobo_QHI_2022$Serial_No <- substr(hobo$SerialID, 54, 61)
 
-hobo_summary <- ggplot(hobo_QHI_2022, aes(x = Datetime_UTC, y = Value)) +
-  geom_line(aes(color=SerialID)) + 
-  facet_wrap(~ Variable, scales = "free_y") +
-  theme_classic()
-
-hobo_summary + theme(legend.position = "none")
-str(hobo_QHI_2022)
-
+# drop unnecessary columns and keep only temp data
+# make separate date and hour column 
+# keep only observations post July 27 2022 aka when they were set up 
 hobo_clean_2022 <- hobo_QHI_2022 %>% 
   filter(Variable == "Temperature") %>% 
-  select(-c(End.of.File, Button.Down, Host.Connected, Button.Up, Index)) 
+  select(-c(End.of.File, Button.Down, Host.Connected, Button.Up, Index)) %>% 
+  mutate(Date = lubridate::date(Datetime_UTC)) %>% 
+  mutate(hour = lubridate::hour(Datetime_UTC)) %>% 
+  filter(Date > lubridate::ymd("2022-07-27"))
+# how many sensors 
+length(unique(hobo_clean_2022$Serial_No))
+  
+# summarize by day, group all sensors together 
+hobo_mean_temp_2022 <- hobo_clean_2022 %>%
+  group_by(Date) %>% 
+  summarise(mean_temp = mean(Value, na.rm = TRUE)) %>% 
+  ungroup()
+
+mean(hobo_mean_temp_2022$mean_temp) # 9.853633
+
 
