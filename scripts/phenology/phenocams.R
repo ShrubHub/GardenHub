3379d7ba788347d34c236d57939a35aaab4893f2
@ericazaja <ericazaja@gmail.com>
2022-12-28 15:27:01 +0100

    Erica wrangles QHI phenocams datasheet

--- scripts/phenology/phenocams.R
+++ scripts/phenology/phenocams.R
@@ -11,18 +11,20 @@ library(gridExtra)
 
 # 2. LOADING DATA ----
 KP_phenocams_2021 <- read_csv("data/phenology/phenocam_pics/KP_phenocams_2021.csv")
-QHI_phenocams_2022 <- read_csv("data/phenology/phenocam_pics/QHI_phenocams_2022.csv")
 KP_phenocams_2022 <- read_csv("data/phenology/phenocam_pics/KP_phenocams_2022.csv")
-Phenocam_Datasheet_QHI <- read_excel("data/phenology/phenocam_pics/Phenocam_Datasheet_QHI.xlsx")
+QHI_phenocams_2022 <- read_csv("data/phenology/phenocam_pics/QHI_phenocams_2022.csv")
+Phenocam_Datasheet_QHI <- read_csv("data/phenology/phenocam_pics/Phenocam_Datasheet_QHI.csv")
 # add common garden datasheet when available
 
 
 # 3. DATA WRANGLING-----
 
+# 3.1. KP ------
 KP_phenocams_2022 <-KP_phenocams_2022[1:9,] # removing loads of NAs
 KP_phenocams_2021 <- KP_phenocams_2021[1:9,]
 
 # renaming columns 
+# 2022 KP data
 KP_phenocams_2022_wrangle <- KP_phenocams_2022 %>%
   rename("Plot" = "PLOT", "Plants_first_visible_through_snow" = "Plants first visible through snow",
          "Snow_melt" = "Snow Free Melt Date (>90% plot free of snow)", 
@@ -50,6 +52,7 @@ KP_phenocams_2022_wrangle <- KP_phenocams_2022 %>%
          Salix_pulchra_last_yellow, Salix_rich_bud_burst, Salix_rich_first_yellow, 
          Salix_rich_last_yellow)
 
+# 2021 KP data
 KP_phenocams_2021_wrangle <- KP_phenocams_2021 %>%
   rename("Plot" = "PLOT", "Plants_first_visible_through_snow" = "Plants first visible through snow",
          "Snow_melt" = "Snow Free Melt Date (>90% plot free of snow)", 
@@ -76,6 +79,54 @@ KP_phenocams_2021_wrangle <- KP_phenocams_2021 %>%
          Salix_pulchra_last_yellow, Salix_rich_bud_burst, Salix_rich_first_yellow, 
          Salix_rich_last_yellow)
 
-# merge 2021 and 2022 kp datasets?
+# merge 2021 and 2022 kp datasets
 KP_phenocams_2021_2022 <- bind_rows(KP_phenocams_2021_wrangle, KP_phenocams_2022_wrangle)
+
+# 3.2. QHI ----
+
+# 2022 QHI phenocams. NB only yellowing 
+QHI_phenocams_2022_wrangle <- QHI_phenocams_2022 %>%
+  rename("Plot" = "PLOT", "Plants_first_visible_through_snow" = "Plants first visible through snow",
+         "Snow_melt" = "Snow Free Melt Date (>90% plot free of snow)", 
+         "All_snow_free" = "First 100% snow-free day",
+         "Snow_return_EoS" = "First snow return day - end of season",
+         "Half_snow_cover_EoS" = "50% snow coverge - end of season",
+         "Full_snow_cover_EoS" = "100% snow coverage - end of season",
+         "First_leaf_bud_burst" = "First leaf bud burst",
+         "Half_leaves_green" ="50% Leaves Green",
+         "All_leaves_green" = "100% Leaves Green",
+         "Half_leaves_yellow" = "50% Leaves Yellow",
+         "All_leaves_yellow" = "100% Leaves Yellow",
+         "Salix_spp" = "SALIX SPP",
+         "Salix_bud_burst" = "Salix First Leaf Bud Burst",
+         "Salix_first_yellow"= "Salix First Yellowing of Leaves",
+         "Salix_last_yellow" = "Salix Last Leaf Turns Yellow") %>%
+  select(Plot, Year, Viewshed, NOTES, Snow_melt, All_snow_free, Snow_return_EoS,
+         Half_snow_cover_EoS, Full_snow_cover_EoS, First_leaf_bud_burst, 
+         Half_leaves_green, All_leaves_green, Half_leaves_yellow, 
+         All_leaves_yellow, Salix_spp, Salix_bud_burst, Salix_first_yellow, 
+         Salix_last_yellow)
+
+# past years QHI 
+Phenocam_Datasheet_QHI <- Phenocam_Datasheet_QHI[1:21,] # removing loads of NAs
+
+QHI_phenocams_past_wrangle <- Phenocam_Datasheet_QHI %>%
+  rename("Plot" = "PLOT", "Plants_first_visible_through_snow" = "Plants first visible through snow",
+         "Snow_melt" = "Snow Free Melt Date (>90% plot free of snow)", 
+         "All_snow_free" = "First 100% snow-free day",
+         "Snow_return_EoS" = "First snow return day - end of season",
+         "Half_snow_cover_EoS" = "50% snow coverge - end of season",
+         "Full_snow_cover_EoS" = "100% snow coverage - end of season",
+         "Salix_spp" = "Salix spp (ARC, PUL, or RIC)",
+         "Salix_bud_burst" = "Salix First Leaf Bud Burst",
+         "Salix_first_yellow"= "Salix First Yellowing of Leaves",
+         "Salix_last_yellow" = "Salix Last Leaf Turns Yellow") %>%
+  select(Plot, Year, NOTES, Snow_melt, All_snow_free, Snow_return_EoS,
+         Half_snow_cover_EoS, Full_snow_cover_EoS, 
+         Salix_spp, Salix_bud_burst, Salix_first_yellow, 
+         Salix_last_yellow)
+
+# merge 2022 and past years datasheets
+QHI_phenocams_2016_2022 <- bind_rows(QHI_phenocams_2022_wrangle, QHI_phenocams_past_wrangle)
+
 
