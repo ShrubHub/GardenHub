175ecb655542ea52e9124b027ec5f10cae47efbf
@ericazaja <ericazaja@gmail.com>
2022-12-28 14:52:52 +0100

    Erica wrangles KP phenocam datasheet

--- scripts/phenology/phenocams.R
+++ scripts/phenology/phenocams.R
@@ -22,7 +22,60 @@ Phenocam_Datasheet_QHI <- read_excel("data/phenology/phenocam_pics/Phenocam_Data
 KP_phenocams_2022 <-KP_phenocams_2022[1:9,] # removing loads of NAs
 KP_phenocams_2021 <- KP_phenocams_2021[1:9,]
 
-# wrangle more before merging 
+# renaming columns 
+KP_phenocams_2022_wrangle <- KP_phenocams_2022 %>%
+  rename("Plot" = "PLOT", "Plants_first_visible_through_snow" = "Plants first visible through snow",
+         "Snow_melt" = "Snow Free Melt Date (>90% plot free of snow)", 
+         "All_snow_free" = "First 100% snow-free day",
+         "First_greening" = "First signs of greening",
+         "Snow_return_EoS" = "First snow return day - end of season",
+         "Half_snow_cover_EoS" = "50% snow coverge - end of season",
+         "Full_snow_cover_EoS" = "100% snow coverage - end of season",
+         "First_leaf_bud_burst" = "First leaf bud burst",
+         "Half_leaves_green" ="50% Leaves Green",
+         "All_leaves_green" = "100% Leaves Green",
+         "First_leaf_yellow"= "First leaf yellow",
+         "Half_leaves_yellow" = "50% Leaves Yellow",
+         "All_leaves_yellow" = "100% Leaves Yellow",
+         "Salix_pulchra_bud_burst" = "Salix pulchra First Leaf Bud Burst",
+         "Salix_pulchra_first_yellow"= "Salix pulchra First Yellowing of Leaves",
+         "Salix_pulchra_last_yellow" = "Salix pulchra Last Leaf Turns Yellow",
+         "Salix_rich_bud_burst" = "Salix richardsonii First Leaf Bud Burst",
+         "Salix_rich_first_yellow" = "Salix richardsonii First Yellowing of Leaves",
+         "Salix_rich_last_yellow" = "Salix richardsonii Last Leaf Turns Yellow") %>%
+  select(Plot, Year, Viewshed, NOTES, Snow_melt, All_snow_free, First_greening, Snow_return_EoS,
+         Half_snow_cover_EoS, Full_snow_cover_EoS, First_leaf_bud_burst, 
+         Half_leaves_green, All_leaves_green, First_leaf_yellow, Half_leaves_yellow, 
+         All_leaves_yellow, Salix_pulchra_bud_burst, Salix_pulchra_first_yellow, 
+         Salix_pulchra_last_yellow, Salix_rich_bud_burst, Salix_rich_first_yellow, 
+         Salix_rich_last_yellow)
+
+KP_phenocams_2021_wrangle <- KP_phenocams_2021 %>%
+  rename("Plot" = "PLOT", "Plants_first_visible_through_snow" = "Plants first visible through snow",
+         "Snow_melt" = "Snow Free Melt Date (>90% plot free of snow)", 
+         "All_snow_free" = "First 100% snow-free day",
+         "Snow_return_EoS" = "First snow return day - end of season",
+         "Half_snow_cover_EoS" = "50% snow coverge - end of season",
+         "Full_snow_cover_EoS" = "100% snow coverage - end of season",
+         "First_leaf_bud_burst" = "First leaf bud burst",
+         "Half_leaves_green" ="50% Leaves Green",
+         "All_leaves_green" = "100% Leaves Green",
+         "First_leaf_yellow"= "First leaf yellow",
+         "Half_leaves_yellow" = "50% Leaves Yellow",
+         "All_leaves_yellow" = "100% Leaves Yellow",
+         "Salix_pulchra_bud_burst" = "Salix pulchra First Leaf Bud Burst",
+         "Salix_pulchra_first_yellow"= "Salix pulchra First Yellowing of Leaves",
+         "Salix_pulchra_last_yellow" = "Salix pulchra Last Leaf Turns Yellow",
+         "Salix_rich_bud_burst" = "Salix richardsonii First Leaf Bud Burst",
+         "Salix_rich_first_yellow" = "Salix richardsonii First Yellowing of Leaves",
+         "Salix_rich_last_yellow" = "Salix richardsonii Last Leaf Turns Yellow") %>%
+  select(Plot, Year, Viewshed, NOTES, Snow_melt, All_snow_free, Snow_return_EoS,
+         Half_snow_cover_EoS, Full_snow_cover_EoS, First_leaf_bud_burst, 
+         Half_leaves_green, All_leaves_green, First_leaf_yellow, Half_leaves_yellow, 
+         All_leaves_yellow, Salix_pulchra_bud_burst, Salix_pulchra_first_yellow, 
+         Salix_pulchra_last_yellow, Salix_rich_bud_burst, Salix_rich_first_yellow, 
+         Salix_rich_last_yellow)
+
 # merge 2021 and 2022 kp datasets?
-KP_phenocams_2021_2022 <- rbind(KP_phenocams_2021, KP_phenocams_2022, by = "PLOT")
+KP_phenocams_2021_2022 <- bind_rows(KP_phenocams_2021_wrangle, KP_phenocams_2022_wrangle)
 
