5f7c7ab70686cb64138dd55a22aa1932acb1a664
@ericazaja <ericazaja@gmail.com>
2023-01-02 13:15:24 +0100

    E formats phenocam dates

--- scripts/phenology/phenocams.R
+++ scripts/phenology/phenocams.R
@@ -82,11 +82,37 @@ KP_phenocams_2021_wrangle <- KP_phenocams_2021 %>%
 # merge 2021 and 2022 kp datasets
 KP_phenocams_2021_2022 <- bind_rows(KP_phenocams_2021_wrangle, KP_phenocams_2022_wrangle)
 
+# write csv
+write.csv(KP_phenocams_2021_2022, "data/phenology/phenocam_pics/working_data/KP_phenocams_2021_2022.csv")
+# dates are A MESS. Easier/quicker to edit manually.
+
+# load manually edited data
+KP_phenocams_2021_2022_manual <- read_csv("data/phenology/phenocam_pics/working_data/KP_phenocams_2021_2022_manual.csv")
+str(KP_phenocams_2021_2022_manual)
+
+KP_phenocams_2021_2022_manual$Snow_melt <- as.POSIXct(KP_phenocams_2021_2022_manual$Snow_melt, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$All_snow_free <- as.POSIXct(KP_phenocams_2021_2022_manual$All_snow_free, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$Snow_return_EoS <- as.POSIXct(KP_phenocams_2021_2022_manual$Snow_return_EoS, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$Full_snow_cover_EoS <- as.POSIXct(KP_phenocams_2021_2022_manual$Full_snow_cover_EoS, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$First_leaf_bud_burst <- as.POSIXct(KP_phenocams_2021_2022_manual$First_leaf_bud_burst, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$Half_leaves_green <- as.POSIXct(KP_phenocams_2021_2022_manual$Half_leaves_green, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$All_leaves_green <- as.POSIXct(KP_phenocams_2021_2022_manual$All_leaves_green, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$First_leaf_yellow <- as.POSIXct(KP_phenocams_2021_2022_manual$First_leaf_yellow, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$Half_leaves_yellow <- as.POSIXct(KP_phenocams_2021_2022_manual$Half_leaves_yellow, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$All_leaves_yellow <- as.POSIXct(KP_phenocams_2021_2022_manual$All_leaves_yellow, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$Salix_pulchra_bud_burst <- as.POSIXct(KP_phenocams_2021_2022_manual$Salix_pulchra_bud_burst, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$Salix_pulchra_first_yellow <- as.POSIXct(KP_phenocams_2021_2022_manual$Salix_pulchra_first_yellow, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$Salix_pulchra_last_yellow <- as.POSIXct(KP_phenocams_2021_2022_manual$Salix_pulchra_last_yellow, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$Salix_rich_bud_burst <- as.POSIXct(KP_phenocams_2021_2022_manual$Salix_rich_bud_burst, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$Salix_rich_first_yellow <- as.POSIXct(KP_phenocams_2021_2022_manual$Salix_rich_first_yellow, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$Salix_rich_last_yellow <- as.POSIXct(KP_phenocams_2021_2022_manual$Salix_rich_last_yellow, format = "%d/%m/%Y")
+KP_phenocams_2021_2022_manual$First_greening <- as.POSIXct(KP_phenocams_2021_2022_manual$First_greening, format = "%d/%m/%Y")
+
 # Divide salix pulchra and richardsonii
-KP_phenocams_2021_2022_pulchra <- KP_phenocams_2021_2022 %>%
+KP_phenocams_2021_2022_pulchra <- KP_phenocams_2021_2022_manual %>%
   select(-Salix_rich_bud_burst, -Salix_rich_first_yellow, -Salix_rich_last_yellow)
 
-KP_phenocams_2021_2022_rich <- KP_phenocams_2021_2022 %>%
+KP_phenocams_2021_2022_rich <- KP_phenocams_2021_2022_manual %>%
   select(-Salix_pulchra_bud_burst, -Salix_pulchra_first_yellow, -Salix_pulchra_last_yellow)
 
 
@@ -168,13 +194,36 @@ QHI_phenocams_2016_2022_edit <- QHI_phenocams_2016_2022 %>%
          Salix_bud_burst, Salix_first_yellow, Salix_last_yellow)%>%
   drop_na(Species)
 
+# dates are a big mess. Easier and quicker to edit manually.
+write_csv(QHI_phenocams_2016_2022_edit, "data/phenology/phenocam_pics/working_data/QHI_phenocams_2016_2022_edit.csv" )
+
+# load manual version
+QHI_phenocams_2016_2022_edit_manual <- read_csv("data/phenology/phenocam_pics/working_data/QHI_phenocams_2016_2022_edit_manual.csv")
+str(QHI_phenocams_2016_2022_edit_manual)
+
+# reclass dates
+QHI_phenocams_2016_2022_edit_manual$Snow_melt <- as.POSIXct(QHI_phenocams_2016_2022_edit_manual$Snow_melt, format = "%d/%m/%Y")
+QHI_phenocams_2016_2022_edit_manual$All_snow_free <- as.POSIXct(QHI_phenocams_2016_2022_edit_manual$All_snow_free, format = "%d/%m/%Y")
+QHI_phenocams_2016_2022_edit_manual$Snow_return_EoS <- as.POSIXct(QHI_phenocams_2016_2022_edit_manual$Snow_return_EoS, format = "%d/%m/%Y")
+QHI_phenocams_2016_2022_edit_manual$Half_snow_cover_EoS <- as.POSIXct(QHI_phenocams_2016_2022_edit_manual$Half_snow_cover_EoS, format = "%d/%m/%Y")
+QHI_phenocams_2016_2022_edit_manual$Full_snow_cover_EoS <- as.POSIXct(QHI_phenocams_2016_2022_edit_manual$Full_snow_cover_EoS, format = "%d/%m/%Y")
+QHI_phenocams_2016_2022_edit_manual$Salix_bud_burst <- as.POSIXct(QHI_phenocams_2016_2022_edit_manual$Salix_bud_burst, format = "%d/%m/%Y")
+QHI_phenocams_2016_2022_edit_manual$Salix_first_yellow <- as.POSIXct(QHI_phenocams_2016_2022_edit_manual$Salix_first_yellow, format = "%d/%m/%Y")
+QHI_phenocams_2016_2022_edit_manual$Salix_last_yellow <- as.POSIXct(QHI_phenocams_2016_2022_edit_manual$Salix_last_yellow, format = "%d/%m/%Y")
+
 # dividing species
-QHI_salarc <- QHI_phenocams_2016_2022_edit %>%
+QHI_salarc <- QHI_phenocams_2016_2022_edit_manual %>%
   filter(Species == "Salix arctica")
 
-QHI_salpul <- QHI_phenocams_2016_2022_edit %>%
+QHI_salpul <- QHI_phenocams_2016_2022_edit_manual %>%
   filter(Species == "Salix pulchra")
 
-QHI_salric <- QHI_phenocams_2016_2022_edit %>%
+QHI_salric <- QHI_phenocams_2016_2022_edit_manual %>%
   filter(Species == "Salix richardsonii")
 # Nb Salix richardsonii only has first yellowing from 2022
+
+
+
+
+
+
