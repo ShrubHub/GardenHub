64bd8652aa7602209876df1ef70d546721073a42
madanderson <your_email@example.com>
2023-02-02 14:58:07 -0500

    updating climate extraction

--- scripts/environment/climate_extract.R
+++ scripts/environment/climate_extract.R
@@ -18,9 +18,30 @@ library(ggpubr)
 library(corrplot)
 library(Hmisc)
 
+
+#### CLIMATE DATA EXTRACTION - CHELSA
+### extraction script
+### By Erica Zaja, created on 26/01/2023
+## Last updated: 02/02/2023 by Madi
+# Script credit: adapted from Joseph Everest and Mariana Garcia Criado
+
+# LOADING LIBRARIES -----
+library(sp)
+library(rgdal)
+library(raster)
+library(ggplot2)
+library(viridis)
+library(rasterVis)
+library(lme4)
+library(sjPlot)
+library(dplyr)
+library(gridExtra)
+library(ggpubr)
+library(corrplot)
+library(Hmisc)
+
 # set wd to external hard drive where data is stored (too big for github)
 setwd("/Volumes/Spectral_21/CHELSA_data_1999-2019")
-
 
 # Climate data from CHELSA 2022
 temp <- raster("data/environment/CHELSA/CHELSA_bio10_10.tif") 
@@ -28,18 +49,19 @@ temp <- raster("data/environment/CHELSA/CHELSA_bio10_10.tif")
 
 # Madi testing something out 
 # files stored on external hard drive because quite large 
-temp_2019 <- raster("CHELSA_data_1999-2019/data/CHELSA_tas_07_2019_V.2.1.tif") 
+temp_2018 <- raster("data/CHELSA_tas_07_2018_V.2.1.tif") 
+temp_1999 <- raster("data/CHELSA_tas_07_1999_V.2.1.tif") 
 
 
-precip <- raster("data/environment/CHELSA/CHELSA_bio10_18.tif")
+precip_2018 <- raster("data/CHELSA_pr_07_2018_V.2.1.tif")
 # Precipitation climatologies: mean monthly precipitation amount of the warmest quarter (bio18) (kg m-2)
 
 # DATA EXPLORATION -----
 # checking resolution of rasters
 res(temp)
-res(temp_2019)
+res(temp_2018)
 
-res(precip)
+res(precip_2018)
 # [1] 0.008333333 0.008333333
 # The spatial resolution of a raster refers the size of each cell in meters. 
 # This size in turn relates to the area on the ground that the pixel represents.
@@ -47,27 +69,33 @@ res(precip)
 
 # Visualising climate rasters
 plot(temp, main = "Mean daily mean air temperatures of the warmest quarter (°C)")
-plot(temp_2019, main = "Mean daily mean air temperatures of 2019 (°C)")
+plot(temp_2018, main = "Mean daily mean air temperatures of 2018 (°C)")
+plot(temp_1999, main = "Mean daily mean air temperatures of 1999 (°C)")
 
-plot(precip, main = "Mean monthly precipitation of the warmest quarter (kg m-2)")
-precip_raster <- levelplot(precip)
+
+
+plot(precip_2018, main = "Mean monthly precipitation of the warmest quarter (kg m-2)")
+precip_2018_raster <- levelplot(precip_2018)
 temp_raster <- levelplot(temp)
 
-temp_raster <- levelplot(temp_2019)
+temp_2018_raster <- levelplot(temp_2018)
 
 # EXTRACTION (to be filled in) ------
 # Loading the coordinates of the sites of interest
-coords <- read.csv("CHELSA_data_1999-2019/lat_long_chelsa.csv") %>% 
+coords <- read.csv("lat_long_chelsa.csv") %>% 
   dplyr::select(longitude, latitude) # keeping lat and long
 
 # Creating SpatialPoints (sp) object of unique coordinates
 coords_sp <- SpatialPoints(coords)
 
 # creating raster stack
-chelsa.stack <- stack(precip, temp)
+chelsa.stack <- stack(precip_2018, temp_2018)
 
 # Extracting variables values for each pair of coordinates
 chelsa.extract <- raster::extract(chelsa.stack, coords_sp, df = TRUE) # extract coords 
+
+tea_climate <- extract(temp_2018, coords)
+
 
 # Combining dataframes:
 # Converting the SpatialPoints (sp) object into a dataframe 
@@ -82,12 +110,13 @@ coord.chelsa.combo.a <- left_join(chelsa.extract, coord.df, by = c("ID" = "ID"))
 
 # Modifying some of the variables to more useful values
 coord.chelsa.combo.b <- coord.chelsa.combo.a %>% 
-  mutate(CHELSA_bio10_10 = CHELSA_bio10_10/10) # Divide by 10 to get to degC
+  mutate((CHELSA_tas_07_2018_V.2.1 = CHELSA_tas_07_2018_V.2.1/10 - 273.15)) # Divide by 10 to get to degC
+# subtract 273.15 to adjust for Kelvin to C conversion 
 
 # Renaming the variables to shorter column headings
 coord.chelsa.combo.c <- coord.chelsa.combo.b %>% 
-  rename(CH_TempMeanSummer = CHELSA_bio10_10,
-         CH_PrecipMeanSummer = CHELSA_bio10_18) %>% na.omit()
+  rename(CH_TempMeanSummer = CHELSA_tas_07_2018_V.2.1,
+         CH_PrecipMeanSummer = CHELSA_pr_07_2018_V.2.1) %>% na.omit()
 
 unique(coord.chelsa.combo.c$CH_TempMeanSummer)
 
