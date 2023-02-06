440b70d19f50cf60468274368e69fd3a97db9f17
@ericazaja <ericazaja@gmail.com>
2023-02-06 17:45:10 +0000

    Erica calculates chelsa means

--- scripts/environment/climate_extract.R
+++ scripts/environment/climate_extract.R
@@ -171,3 +171,63 @@ july_enviro <- full_join(mean_prec_long, mean_temp_data,
 
 write.csv(july_enviro, "july_enviro_chelsa.csv")
 
+# Loading chelsa temperature and precipitation data -----
+july_enviro_chelsa <- read_csv("data/july_enviro_chelsa.csv")
+
+
+# DATA WRANGLE ------
+unique(july_enviro_chelsa$site)
+
+#rename column
+july_enviro_chelsa <- july_enviro_chelsa %>%
+  rename ("mean_temp_C" ="(mean_temp_C = (mean_temp/10 - 273.15))")
+
+# QHI july mean temp and precip
+QHI_july_temp <- july_enviro_chelsa %>%
+  filter(site == "QHI")%>%
+  select(site, year, mean_temp_C)
+
+mean(QHI_july_temp$mean_temp_C, na.rm=TRUE) # 6.15
+# QHI july mean surface temp: 9.10 °C 
+# based on 2022 TOMST and 2017 HOBO data 
+
+QHI_july_precip <- july_enviro_chelsa %>%
+  filter(site == "QHI")%>%
+  select(site, year, PrecipMeanJuly)
+
+mean(QHI_july_precip$PrecipMeanJuly, na.rm=TRUE) # 9586.81
+# QHI july mean surface temp: 9.10 °C 
+
+# KP july mean temp and precip
+KP_july_temp <- july_enviro_chelsa %>%
+  filter(site == "Kluane_plateau")%>%
+  select(site, year, mean_temp_C)
+
+mean(KP_july_temp$mean_temp_C, na.rm=TRUE) #7.311905
+
+KP_july_precip <- july_enviro_chelsa %>%
+  filter(site == "Kluane_plateau")%>%
+  select(site, year, PrecipMeanJuly)
+
+mean(KP_july_precip$PrecipMeanJuly, na.rm=TRUE) # 7123.429
+
+# CG july mean temp and precip
+CG_july_temp <- july_enviro_chelsa %>%
+  filter(site == "Common_garden") %>%
+  select(site, year, mean_temp_C)
+
+mean(CG_july_temp$mean_temp_C, na.rm=TRUE) #13.67857
+
+# Common garden july mean surface temp: 17.97°C 
+# Based on 6 years data (TOMST+ HOBO)
+
+CG_july_precip <- july_enviro_chelsa %>%
+  filter(site == "Common_garden") %>%
+  select(site, year, PrecipMeanJuly)
+
+mean(CG_july_precip$PrecipMeanJuly, na.rm=TRUE) # 5287.333
+
+
+
+
+
