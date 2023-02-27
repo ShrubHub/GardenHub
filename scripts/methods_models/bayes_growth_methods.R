681614157d1868d5ae4cd4fed37e06cd4532f23d
@ericazaja <ericazaja@gmail.com>
2023-02-27 11:05:18 +0000

    E runs method growth models NB a couple of arctica models dont converge

--- scripts/methods_models/bayes_growth_methods.R
+++ scripts/methods_models/bayes_growth_methods.R
@@ -16,7 +16,7 @@ unique_source_mother$SampleYear <- as.factor(unique_source_mother$SampleYear)
 unique_source_mother$Species <- as.factor(unique_source_mother$Species)
 unique_source_mother$Site <- as.factor(unique_source_mother$Site)
 unique_source_mother$year <-  format(as.Date(unique_source_mother$SampleDate, format="%d/%m/%Y"),"%Y")
-unique(unique_source_mother$year)
+unique(unique_source_mother$SampleYear)
 
 # Species specific datasets
 unique_source_mother_rich <- unique_source_mother %>%
@@ -28,59 +28,172 @@ unique_source_mother_pulchra <- unique_source_mother %>%
 unique_source_mother_arctica <- unique_source_mother %>%
   filter(Species == "Salix arctica")
 
+# exploring variables distribution
+hist(unique_source_mother_arctica$Canopy_Height_cm) # right skew
+hist(unique_source_mother_arctica$mean_width) # right skew
+hist(unique_source_mother_arctica$mean_stem_elong) # right skew
+hist(unique_source_mother_arctica$Stem_diameter, breaks = 30) #  right skew - so weird
+hist(unique_source_mother_arctica$biovolume,  breaks = 30)#  right skew
+
 # 4. Modelling -----
 
-
-# a. Canopy height -----
+# a. CANOPY HEIGHT -----
 # Salix richardsonii -------
-source_rich_height <- brms::brm(log(Canopy_Height_cm) ~ Site + (1|year),
+source_rich_height <- brms::brm(log(Canopy_Height_cm) ~ Site + (1|SampleYear),
                                 data = unique_source_mother_rich, family = gaussian(), chains = 3,
-                                iter = 3000, warmup = 1000)
+                                iter = 3000, warmup = 1000, 
+                                control = list(max_treedepth = 15, adapt_delta = 0.99))
 
-summary(source_rich_height)
+summary(source_rich_height) # lower heights for QHi
 plot(source_rich_height)
-pp_check(source_rich_height) 
+pp_check(source_rich_height, type = "dens_overlay", nsamples = 100)  # good) 
 
+# Salix pulchra ------
+source_pul_height <- brms::brm(log(Canopy_Height_cm) ~ Site + (1|SampleYear),
+                                data = unique_source_mother_pulchra, family = gaussian(), chains = 3,
+                                iter = 3000, warmup = 1000, 
+                                control = list(max_treedepth = 15, adapt_delta = 0.99))
 
-# b. Stem elongation ---- 
+summary(source_pul_height) # lower heights for QHi
+plot(source_pul_height)
+pp_check(source_pul_height, type = "dens_overlay", nsamples = 100)  # good) 
+
+# Salix arctica -----
+source_arc_height <- brms::brm(log(Canopy_Height_cm) ~ Site + (1|SampleYear),
+                                data = unique_source_mother_arctica, family = gaussian(), chains = 3,
+                                iter = 3000, warmup = 1000, 
+                                control = list(max_treedepth = 15, adapt_delta = 0.99))
+
+summary(source_arc_height) # no significant difference
+plot(source_arc_height)
+pp_check(source_arc_height, type = "dens_overlay", nsamples = 100)  # good) 
+
+# b. STEM ELONGATION ---- 
 # Salix richardsonii -------
-source_rich_elong <- brms::brm(log(mean_stem_elong) ~ Site + (1|year),
+source_rich_elong <- brms::brm(log(mean_stem_elong) ~ Site + (1|SampleYear),
                                 data = unique_source_mother_rich, family = gaussian(), chains = 3,
-                                iter = 3000, warmup = 1000)
+                                iter = 3000, warmup = 1000, 
+                               control = list(max_treedepth = 15, adapt_delta = 0.99))
 
-summary(source_rich_elong)
+summary(source_rich_elong)# shorter elongations for QHi
 plot(source_rich_elong)
-pp_check(source_rich_elong)
+pp_check(source_rich_elong,  type = "dens_overlay", nsamples = 100)
 
-# c. Width----
+# Salix pulchra -------
+source_pul_elong <- brms::brm(log(mean_stem_elong) ~ Site + (1|SampleYear),
+                               data = unique_source_mother_pulchra, family = gaussian(), chains = 3,
+                               iter = 5000, warmup = 1000, 
+                               control = list(max_treedepth = 15, adapt_delta = 0.99))
+
+summary(source_pul_elong)# shorter elongations for QHi
+plot(source_pul_elong)
+pp_check(source_pul_elong,  type = "dens_overlay", nsamples = 100)
+
+# Salix arctica: NOT CONVERGED------
+source_arc_elong <- brms::brm(log(mean_stem_elong) ~ Site + (1|SampleYear),
+                               data = unique_source_mother_arctica, family = gaussian(), chains = 3,
+                               iter = 5000, warmup = 1000, 
+                               control = list(max_treedepth = 20, adapt_delta = 0.99))
+
+summary(source_arc_elong)# shorter elongations for QHi
+plot(source_arc_elong)
+pp_check(source_arc_elong,  type = "dens_overlay", nsamples = 100)
+
+# c. WIDTH ----
 # Salix richardsonii -------
-source_rich_width<- brms::brm(log(mean_width) ~ Site,
+source_rich_width<- brms::brm(log(mean_width) ~ Site + (1|SampleYear),
                                data = unique_source_mother_rich, family = gaussian(), chains = 3,
-                               iter = 3000, warmup = 1000)
+                               iter = 3000, warmup = 1000, 
+                              control = list(max_treedepth = 15, adapt_delta = 0.99))
 
-summary(source_rich_elong)
-plot(source_rich_elong)
-pp_check(source_rich_elong) 
-conditional_effects(source_rich_elong)
+summary(source_rich_width) # significantly LARGER width for QHI shrubs
+plot(source_rich_width)
+pp_check(source_rich_width, type = "dens_overlay", nsamples = 100)  # fine
+
+# Salix pulchra -----
+source_pul_width<- brms::brm(log(mean_width) ~ Site + (1|SampleYear),
+                              data = unique_source_mother_pulchra, family = gaussian(), chains = 3,
+                              iter = 3000, warmup = 1000, 
+                              control = list(max_treedepth = 15, adapt_delta = 0.99))
+
+summary(source_pul_width) # significantly lower width for QHI shrubs
+plot(source_pul_width)
+pp_check(source_pul_width, type = "dens_overlay", nsamples = 100)  # fine
 
 
-# d. Stem diameter -----
+# Salix arctica -------
+source_arc_width<- brms::brm(log(mean_width) ~ Site + (1|SampleYear),
+                             data = unique_source_mother_arctica, family = gaussian(), chains = 3,
+                             iter = 3000, warmup = 1000, 
+                             control = list(max_treedepth = 15, adapt_delta = 0.99))
+
+summary(source_arc_width) # no significant diff
+plot(source_arc_width)
+pp_check(source_arc_width, type = "dens_overlay", nsamples = 100)  # fine
+
+
+# d. STEM DIAMETER -----
 # Salix richardsonii -------
-source_rich_diam <- brms::brm(log(Stem_diameter) ~ Site,
+source_rich_diam <- brms::brm(log(Stem_diameter) ~ Site + (1|SampleYear),
                               data = unique_source_mother_rich, family = gaussian(), chains = 3,
-                              iter = 3000, warmup = 1000)
+                              iter = 3000, warmup = 1000, 
+                              control = list(max_treedepth = 15, adapt_delta = 0.99))
 
-summary(source_rich_diam)
+summary(source_rich_diam) # lower stem diameter for QHi shrubs
 plot(source_rich_diam)
-pp_check(source_rich_diam) 
+pp_check(source_rich_diam, type = "dens_overlay", nsamples = 100) 
 
-# d. Biovolume ----
+# Salix pulchra ------
+source_pul_diam <- brms::brm(log(Stem_diameter) ~ Site + (1|SampleYear),
+                              data = unique_source_mother_pulchra, family = gaussian(), chains = 3,
+                              iter = 3000, warmup = 1000, 
+                              control = list(max_treedepth = 15, adapt_delta = 0.99))
+
+summary(source_pul_diam) # lower stem diameter for QHi shrubs
+plot(source_pul_diam)
+pp_check(source_pul_diam, type = "dens_overlay", nsamples = 100) 
+
+# Salix arctica :NOT CONVERGED------
+source_arc_diam <- brms::brm(log(Stem_diameter) ~ Site + (1|SampleYear),
+                              data = unique_source_mother_arctica, family = gaussian(), chains = 3,
+                              iter = 3000, warmup = 1000, 
+                              control = list(max_treedepth = 15, adapt_delta = 0.99))
+
+summary(source_arc_diam) # lower stem diameter for QHi shrubs
+plot(source_arc_diam)
+pp_check(source_arc_diam, type = "dens_overlay", nsamples = 100) 
+
+# d.BIOVOLUME----
 # Salix richardsonii -------
-source_rich_biovol <- brms::brm(log(biovolume) ~ Site + (1|year),
+source_rich_biovol <- brms::brm(log(biovolume) ~ Site + (1|SampleYear),
                              data = unique_source_mother_rich, family = gaussian(), chains = 3,
-                             iter = 3000, warmup = 1000)
+                             iter = 3000, warmup = 1000, 
+                             control = list(max_treedepth = 15, adapt_delta = 0.99))
 
-summary(source_rich_biovol)
+
+summary(source_rich_biovol) # no significant difference
 plot(source_rich_biovol)
-pp_check(source_rich_biovol) 
+pp_check(source_rich_biovol, type = "dens_overlay", nsamples = 100) 
+
+# Salix pulchra -------
+source_pul_biovol <- brms::brm(log(biovolume) ~ Site + (1|SampleYear),
+                                data = unique_source_mother_pulchra, family = gaussian(), chains = 3,
+                                iter = 3000, warmup = 1000, 
+                                control = list(max_treedepth = 15, adapt_delta = 0.99))
+
+
+summary(source_pul_biovol) # smaller biovolume for QHI shrubs
+plot(source_pul_biovol)
+pp_check(source_pul_biovol, type = "dens_overlay", nsamples = 100) 
+
+# Salix arctica -------
+source_arc_biovol <- brms::brm(log(biovolume) ~ Site + (1|SampleYear),
+                               data = unique_source_mother_arctica, family = gaussian(), chains = 3,
+                               iter = 3000, warmup = 1000, 
+                               control = list(max_treedepth = 15, adapt_delta = 0.99))
+
+
+summary(source_arc_biovol) # no significant difference
+plot(source_arc_biovol)
+pp_check(source_arc_biovol, type = "dens_overlay", nsamples = 100) 
 
