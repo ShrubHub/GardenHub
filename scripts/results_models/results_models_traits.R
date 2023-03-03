a8bc95477d7551b91d74c327ae5747e5ac738996
madanderson <your_email@example.com>
2023-03-02 19:46:12 -0500

    ommitting old filter

--- scripts/results_models/results_models_traits.R
+++ scripts/results_models/results_models_traits.R
@@ -39,13 +39,6 @@ all_CG_source_traits$date_sampled <- as.POSIXct(all_CG_source_traits$date_sample
 all_CG_source_traits$year <- as.factor(all_CG_source_traits$year)
 # currently doesn't exist! all_CG_source_traits$Sample_age <- as.factor(all_CG_source_traits$Sample_age)
 
-# filter out two extreme LDMC values from 2014 
-# filter out one extreme LMA value from 2021 
-all_CG_source_traits <- all_CG_source_traits %>% 
-  filter(LDMC_g_g < 1 | is.na(LDMC_g_g)) %>% 
-  filter(leaf_mass_per_area_g_m2 < 130 | is.na(leaf_mass_per_area_g_m2)) %>% 
-  filter(SLA > 2 | is.na(SLA))
-
 # to run separate models per species filter out species: 
 arctica_all_traits <- all_CG_source_traits %>% 
   filter(Species == "Salix arctica")
