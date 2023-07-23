b075a57b4dc8ff6d54a55bc8fa50f3ec252c709f
madanderson <your_email@example.com>
2023-07-23 16:00:20 -0700

    adding 2023 data as csv

---
+++ scripts/common_garden/common_garden_2023.R
@@ -0,0 +1,11 @@
+# Common garden data 2023 
+# By Madelaine Anderson 
+# 23 July 2023 
+
+# libraries ----
+library(tidyr)
+library(ggplot2)
+
+# data 
+data_2023 <- read.csv("data/common_garden_data_2023/Common_Garden_data_Jul2023_final.csv")
+
