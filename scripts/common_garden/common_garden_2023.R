4f2acd7ab363b811e27bed053f647c8488c20a8f
madanderson <your_email@example.com>
2023-07-24 13:52:39 -0700

    madi merging data 2023

--- scripts/common_garden/common_garden_2023.R
+++ scripts/common_garden/common_garden_2023.R
@@ -9,3 +9,79 @@ library(ggplot2)
 # data 
 data_2023 <- read.csv("data/common_garden_data_2023/Common_Garden_data_Jul2023_final.csv")
 
+data_2023_wrangle <- data_2023 %>% 
+  dplyr::rename("Sample_ID" = "Name", 
+                "Sample_Date" = "Date..dd.mm.yyyy.", 
+                "Canopy_Height_Aug" = "Height_Aug2022..cm.",
+                 "Canopy_Height_cm" = "Height..cm.",  
+                "Width_cm" = "Width..cm.", 
+                "Width_2_cm" = "Width.2..cm.", 
+                "Stem_Elongation_1_mm" = "Stem.Elongation.1..mm.", 
+                "Stem_Elongation_2_mm" = "Stem.Elongation.2..mm.", 
+                'Stem_Elongation_3_mm' = "Stem.Elongation.3..mm.",
+                "Length_1_mm" = "Length.1..mm.", 
+                "Length_2_mm" = "Length.2..mm.", 
+                "Length_3_mm" = "Length.3..mm.", 
+                "Stem_diameter" = "Stem.diameter", 
+                "July_2023_notes" = "X11.July.2023.notes") %>% 
+  dplyr::select(-c(Canopy_Height_Aug, Sample_Date))
+
+str(data_2023_wrangle)
+# reclass variables as numeric
+data_2023_wrangle$Canopy_Height_cm <- as.numeric(data_2023_wrangle$Canopy_Height_cm)
+data_2023_wrangle$Stem_Elongation_1_mm <- as.numeric(data_2023_wrangle$Stem_Elongation_1_mm)
+data_2023_wrangle$Stem_Elongation_2_mm <- as.numeric(data_2023_wrangle$Stem_Elongation_2_mm)
+data_2023_wrangle$Stem_Elongation_3_mm <- as.numeric(data_2023_wrangle$Stem_Elongation_3_mm)
+data_2023_wrangle$Length_1_mm <- as.numeric(data_2023_wrangle$Length_1_mm)
+data_2023_wrangle$Length_2_mm <- as.numeric(data_2023_wrangle$Length_2_mm)
+data_2023_wrangle$Length_3_mm <- as.numeric(data_2023_wrangle$Length_3_mm)
+data_2023_wrangle$Stem_diameter <- as.numeric(data_2023_wrangle$Stem_diameter)
+data_2023_wrangle$Year <- as.numeric(data_2023_wrangle$Year)
+data_2023_wrangle$Month <- as.numeric(data_2023_wrangle$Month)
+data_2023_wrangle$Day <- as.numeric(data_2023_wrangle$Day)
+
+# make mean leaf length, mean stem elongation, and biovolume columns 
+data_2023_wrangle <- data_2023_wrangle %>% 
+  mutate(mean_stem_elong = ((Stem_Elongation_1_mm + Stem_Elongation_2_mm + Stem_Elongation_3_mm)/3), 
+         mean_leaf_length = ((Length_1_mm + Length_2_mm + Length_3_mm)/3),
+         mean_width = ((Width_cm + Width_2_cm)/2)) %>% 
+  mutate(biovolume = (Canopy_Height_cm* Width_cm* Width_2_cm))
+
+# make standard sample ID column to avoid issue of dashes, spaces, etc. 
+data_2023_wrangle$SampleID_standard <- toupper(data_2023_wrangle$Sample_ID) # make all uppercase characters 
+data_2023_wrangle$SampleID_standard<-gsub("-","",as.character(data_2023_wrangle$SampleID_standard)) # remove "-"
+data_2023_wrangle$SampleID_standard<-gsub(" ","",as.character(data_2023_wrangle$SampleID_standard)) # remove spaces " " 
+
+# date column is reading wrong, manually make day month year columns bc measurements were collected 23 July 2023
+# also add site column
+data_2023_wrangle$Year <- "2023"
+data_2023_wrangle$Month <- "07"
+data_2023_wrangle$Day <- "23"
+data_2023_wrangle$Site <- "Common_garden"
+
+# try to merge with complete dataset from 2022 
+# wish me luck 
+all_CG_source_growth <- read_csv("data/all_CG_source_growth.csv")
+# to get year planted and species, extract from 2022 dataset 
+spp_year <- all_CG_source_growth %>% 
+  dplyr::select(c(Species, Year_planted, SampleID_standard)) %>% 
+  distinct()
+
+all_data_merge <- full_join(all_CG_source_growth, data_2023_wrangle, 
+                           by = c("Bed", "Site",
+                                  "Canopy_Height_cm",
+                                  "Width_cm", "Width_2_cm", 
+                                  "Stem_Elongation_1_mm", "Stem_Elongation_2_mm", "Stem_Elongation_3_mm", 
+                                  "Length_1_mm", "Length_2_mm", "Length_3_mm",
+                                  "Stem_diameter", 
+                                  "mean_stem_elong", 
+                                  "mean_leaf_length", 
+                                  "biovolume", "mean_width", 
+                                  "SampleID_standard", 
+                                  "Year", "Month", "Day")) 
+
+all_data_wrangle <- all_data_merge %>% 
+  dplyr::select(-Sample_age) %>% 
+  mutate(Sample_age = Year - Year_planted)
+  
+
